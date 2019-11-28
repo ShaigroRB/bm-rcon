72925a4cd114e30ae53440c4d7e933d4863c8afe
ShaigroRB <why_not.shaigro@yahoo.fr>
2019-11-28 23:42:24 +0100

    :tada: Initial commit

---
+++ BM-RCON/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BM_RCON
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

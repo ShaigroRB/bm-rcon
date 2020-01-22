7f379ffd31e681668ab0fe6ace919c773b3069d4
ShaigroRB <why_not.shaigro@yahoo.fr>
2020-01-22 17:59:15 +0100

    :sparkles: created BM_RCON class

---
+++ BM-RCON/BM_RCON_lib/BM_RCON.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BM_RCON.BM_RCON_lib
+{
+    class BM_RCON
+    {
+        string address;
+        int port;
+        string password;
+
+        public BM_RCON(string addr, int port, string password)
+        {
+            this.address = addr;
+            this.port = port;
+            this.password = password;
+        }
+    }
+}

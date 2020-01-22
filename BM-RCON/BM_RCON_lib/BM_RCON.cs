e83779bed0fe3044ae6464a16d5d1e46c839df09
ShaigroRB <why_not.shaigro@yahoo.fr>
2020-01-22 18:21:43 +0100

    :sparkles: create packet given a request type and a body

--- BM-RCON/BM_RCON_lib/BM_RCON.cs
+++ BM-RCON/BM_RCON_lib/BM_RCON.cs
@@ -16,5 +16,34 @@ namespace BM_RCON.BM_RCON_lib
             this.port = port;
             this.password = password;
         }
+
+        public byte[] CreatePacket(RequestType RequestType, string body)
+        {
+            // reqType needs to be an 16-bit integer
+            short req_type = (short)RequestType;
+            UTF8Encoding uTF8 = new UTF8Encoding();
+
+            // convert into bytes
+            byte[] req_type_bytes = BitConverter.GetBytes(req_type);
+            byte[] body_bytes = uTF8.GetBytes(body);
+
+            // final packet
+            // + 1 at the end to add a null byte
+            byte[] final_packet = new byte[req_type_bytes.Length + body_bytes.Length + 1];
+
+            int byte_ptr = 0;
+
+            // copy bytes to final packet
+            req_type_bytes.CopyTo(final_packet, byte_ptr);
+            byte_ptr += req_type_bytes.Length;
+
+            body_bytes.CopyTo(final_packet, byte_ptr);
+            byte_ptr += body_bytes.Length;
+
+            // don't forget the last null byte
+            final_packet[byte_ptr] = (byte)0;
+
+            return final_packet;
+        }
     }
 }

ddaabd7ac9e08efcf7ae80f3b83e0e0a66b1dcdd
ShaigroRB <robin.boucher@epita.fr>
2020-01-25 02:41:17 +0100

    :sparkles: enum for power ups id

---
+++ BM-RCON/BM_RCON_lib/enums/PowerUps.cs
@@ -0,0 +1,12 @@
+﻿namespace BM_RCON.BM_RCON_lib
+{
+    enum PowerUpID
+    {
+        triple_damage = 1,
+        super_speed = 2,
+        regen = 3,
+        invis = 4,
+        // not implemented yet
+        bfg = 5
+    }
+}

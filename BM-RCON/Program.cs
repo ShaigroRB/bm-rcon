ae2dcb7451b2368fe113d1e1ae015ec26717af0d
ShaigroRB <robin.boucher@epita.fr>
2020-01-25 22:06:20 +0100

    :alembic: cleaned up main + added sendRequest (delay before sending)

--- BM-RCON/Program.cs
+++ BM-RCON/Program.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Threading;
 
 // alias for namespace
-using BM_RCON_lib = BM_RCON.BM_RCON_lib;
+using lib = BM_RCON.BM_RCON_lib;
 using RequestType = BM_RCON.BM_RCON_lib.RequestType;
 
 
@@ -17,88 +17,57 @@ namespace BM_RCON
         const string addr = "127.0.0.1";
         const string passwd = "admin";
 
+        private static void sendRequest(lib.BM_RCON rcon, RequestType requestType, string body)
+        {
+            Thread.Sleep(160);
+            rcon.SendRequest(requestType, body);
+        }
+
         static int Main(string[] args)
         {
             try
-            {
-                bool test_lib = true;
-
-                // Test connection
-
-                // type password
-                /* 
-                 String body = String.Empty;
-                 Console.OutputEncoding = Encoding.UTF8;
-                 Console.Write("Type the password of rcon server: ");
-                 body = Console.ReadLine();
-                 //*/
+            {                
                 string body = passwd;
                 // init rcon object
-                BM_RCON_lib.BM_RCON rcon_obj = new BM_RCON_lib.BM_RCON(addr, port, body);
-                if (test_lib)
+                lib.BM_RCON rcon_obj = new lib.BM_RCON(addr, port, body);
+                rcon_obj.Connect();
+                Console.WriteLine("");
+
+                //sendRequest(rcon_obj, RequestType.command, "enablemutators");
+                lib.RCON_Event evt;
+                while (true)
                 {
-                    rcon_obj.Connect();
+                    evt = rcon_obj.ReceiveEvent();
+                    Console.WriteLine("");
 
-                    Thread.Sleep(160);
-                    rcon_obj.ReceiveEvent();
-
-                    Thread.Sleep(160);
-                    rcon_obj.SendRequest(RequestType.command, "enablemutators");
-                    Thread.Sleep(160); // 1
-                    rcon_obj.SendRequest(RequestType.command, "echo \"Success! (1)\" \"255\"");
-                    Thread.Sleep(160); // 2
-                    rcon_obj.SendRequest(RequestType.command, "echo \"Success! (2)\" \"255\"");
-                    Thread.Sleep(160); // 3
-                    rcon_obj.SendRequest(RequestType.command, "echo \"Success! (3)\" \"255\"");
-                    Thread.Sleep(160); // ping
-                    rcon_obj.SendRequest(RequestType.ping, "It's okay.");
-                    Thread.Sleep(160); // 4
-                    rcon_obj.SendRequest(RequestType.command, "echo \"Success! (4)\" \"255\"");
-                    Thread.Sleep(160); // 5
-                    rcon_obj.SendRequest(RequestType.command, "echo \"Success! (5)\" \"255\"");
-                    Thread.Sleep(160); // last request before disconnecting
-
-                    rcon_obj.Disconnect();
-                }
-
-                if (!test_lib)
-                {
-
-                    byte[] finalPacket = rcon_obj.CreatePacket(BM_RCON_lib.RequestType.login, body);
-                    TcpClient client = new TcpClient(addr, port);
-                    NetworkStream stream = client.GetStream();
-                    stream.ReadTimeout = 4000;
-
-                    stream.Write(finalPacket, 0, finalPacket.Length);
-
-                    Console.WriteLine("Packet with password sent.");
-                    Console.WriteLine("Final packet: " + System.Text.Encoding.UTF8.GetString(finalPacket));
-
-
-                    string msg = " confetti";
-
-                    while (true)
+                    if (evt.EventID == (short)lib.EventType.player_spawn)
                     {
-                        Thread.Sleep(160);
-                        byte[] packet_received = new byte[client.ReceiveBufferSize];
-                        if (client.ReceiveBufferSize > 0)
-                        {
-                            int tmp = stream.Read(packet_received, 0, packet_received.Length);
-                            msg = System.Text.Encoding.UTF8.GetString(packet_received, 0, tmp);
-                            Console.WriteLine("Packet received: {0}", msg);
-                        }
+                        Console.WriteLine("Event ID: {0}", evt.EventID.ToString());
+                        Console.WriteLine("JSON:");
+                        dynamic obj = evt.JsonAsObj;
+                        Console.WriteLine("First weapon id: {0}", obj.Weap1.ToString());
+                        Console.WriteLine("Secondary weapon id: {0}", obj.Weap2.ToString());
+                        Console.WriteLine("Grenade slot id: {0}", obj.Equip.ToString());
+
+                        Console.WriteLine("");
+                        Console.WriteLine(evt.JsonAsObj.ToString());
                     }
-                    Thread.Sleep(160);
-                    msg = "test \"sdf\" \"sdfdsf\"";
 
-                    byte[] test = rcon_obj.CreatePacket(BM_RCON_lib.RequestType.command, msg);
-                    stream.Write(test, 0, test.Length);
-                    Console.WriteLine("Command {0} sent.", msg);
-
-                    stream.Close();
-                    client.Close();
+                    if (evt.EventID == (short)lib.EventType.rcon_ping)
+                    {
+                        sendRequest(rcon_obj, RequestType.ping, "I pinged");
+                    }
+                    if (evt.EventID == (short)lib.EventType.player_taunt)
+                    {
+                        break;
+                    }
                 }
-                
+
+                Thread.Sleep(160); // last request before disconnecting
+
+                rcon_obj.Disconnect();
+
+
             }
             catch (Exception e)
             {

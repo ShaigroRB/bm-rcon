87687f6db2134ad9c429129f2d599140c04fe58e
ShaigroRB <robin.boucher@epita.fr>
2020-01-26 01:28:21 +0100

    :construction: created Betmode class

---
+++ BM-RCON/mods/betmode/Betmode.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BM_RCON.mods.betmode
+{
+    class Betmode
+    {
+        static int Main(string[] args)
+        {
+            return 0;
+        }
+    }
+}

1a4b55bc8d3bd02ce8680b6cc90e61e3c217a0f1
ShaigroRB <robin.boucher@epita.fr>
2020-01-29 19:16:33 +0100

    :construction: Player class added

---
+++ BM-RCON/mods/betmode/Player.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BM_RCON.mods.betmode
+{
+    class Player
+    {
+        string name;
+        string profile;
+        // type of vice is the index in the list
+        short[] vices;
+        bool is_connected;
+        bool is_alive;
+
+        public Player(string name, string profile)
+        {
+            this.name = name;
+            this.profile = profile;
+            this.vices = new short[40];
+            // when a player is created, it means the player just connected
+            this.is_connected = true;
+            this.is_alive = false;
+        }
+    }
+}

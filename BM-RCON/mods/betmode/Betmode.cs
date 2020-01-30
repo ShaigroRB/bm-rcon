4f742e4ede1dca16810ffde999147532a66d07a6
ShaigroRB <robin.boucher@epita.fr>
2020-01-30 20:49:07 +0100

    :pencil: docs on what is the betmode + docs on events to catch
    In Main of Betmode class:
    - Explanation of what is the mod
    - Events to catch are:
       * player_connect
       * player_disconnect
       * player_spawn
       * player_death
       * survival_get_vice
       * survival_use_vice
       * survival_new_wave
       * survival_flag_unlocked
       * chat_message
    - Docs for player_connect, player_disconnect, player_spawn and player_death added

--- BM-RCON/mods/betmode/Betmode.cs
+++ BM-RCON/mods/betmode/Betmode.cs
@@ -27,7 +27,33 @@ namespace BM_RCON.mods.betmode
         }
         static int Main(string[] args)
         {
-
+            /*
+             * - you bet the minimum *number of people who will survive through the next wave without dying
+             * - the next wave will spawn the same amount of bosses than the *number bet
+             * - if the bet is won, every player will earn a *number of random vices
+            */
+             /*
+              * the events to catch are:
+              * - player_connect:
+              *     - Check if an instance of Player corresponding to the player exists
+              *     - If not, create one and add it to connected_players
+              *     - Otherwise, move the corresponding Player from disconnected_players to connected_players
+              * - player_disconnect:
+              *     - Set the Player as dead
+              *     - Update the Bet (if one) with the Player
+              *     - Move the Player from connected_players to disconnected_players
+              * - player_spawn:
+              *     - Set the Player as alive
+              * - player_death:
+              *     - Set the Player as dead
+              *     - Update the Bet (if one) with the Player
+              * - survival_get_vice:
+              * - survival_use_vice:
+              * - survival_new_wave
+              * - survival_flag_unlocked
+              * - chat_message
+              * 
+             */
             try
             {
 

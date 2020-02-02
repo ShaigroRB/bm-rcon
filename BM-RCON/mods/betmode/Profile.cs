0370ea32696be9e8c56a091a35fac5268848ac1a
ShaigroRB <robin.boucher@epita.fr>
2020-02-02 22:25:52 +0100

    :sparkles: Profile class created

---
+++ BM-RCON/mods/betmode/Profile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BM_RCON.mods.betmode
+{
+    class Profile
+    {
+        int profileID;
+        int storeID;
+
+        public Profile(int profileID, int storeID)
+        {
+            this.profileID = profileID;
+            this.storeID = storeID;
+        }
+
+        public int ProfileID
+        {
+            get;
+        }
+
+        public int StoreID
+        {
+            get;
+        }
+    }
+}

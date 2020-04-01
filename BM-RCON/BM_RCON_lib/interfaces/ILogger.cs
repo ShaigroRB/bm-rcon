797f87b6ae2cbbc9773a512fde6564a0b56f9de8
ShaigroRB <robin.boucher@epita.fr>
2020-04-01 10:41:36 +0200

    :sparkles: ILogger added with docs

---
+++ BM-RCON/BM_RCON_lib/interfaces/ILogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BM_RCON.BM_RCON_lib
+{
+    interface ILogger
+    {
+        /// <summary>
+        /// Default logging (no type of log specified)
+        /// </summary>
+        /// <param name="msg">Message to log</param>
+        void Log(string msg);
+
+        /// <summary>
+        /// Info logging
+        /// </summary>
+        /// <param name="msg">Message to log</param>
+        void LogInfo(string msg);
+
+        /// <summary>
+        /// Debug logging
+        /// </summary>
+        /// <param name="msg">Message to log</param>
+        void LogDebug(string msg);
+
+        /// <summary>
+        /// Error logging
+        /// </summary>
+        /// <param name="msg">Message to log</param>
+        void LogError(string msg);
+
+        /// <summary>
+        /// Warning logging
+        /// </summary>
+        /// <param name="msg">Message to log</param>
+        void LogWarning(string msg);
+    }
+}

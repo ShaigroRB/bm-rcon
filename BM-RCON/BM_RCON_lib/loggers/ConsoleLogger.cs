3d76485184b2b9a1f569de0967801357669c89e1
ShaigroRB <robin.boucher@epita.fr>
2020-04-01 11:00:14 +0200

    :sparkles: Added ConsoleLogger to output logging to console

---
+++ BM-RCON/BM_RCON_lib/loggers/ConsoleLogger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace BM_RCON.BM_RCON_lib
+{
+    /// <summary>
+    /// Output logging to console
+    /// </summary>
+    class ConsoleLogger : ILogger
+    {
+        public void Log(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public void LogDebug(string msg)
+        {
+            Console.WriteLine("[DEBUG]: {0}", msg);
+        }
+
+        public void LogError(string msg)
+        {
+            Console.WriteLine("[ERROR]: {0}", msg);
+        }
+
+        public void LogInfo(string msg)
+        {
+            Console.WriteLine("[INFO]: {0}", msg);
+        }
+
+        public void LogWarning(string msg)
+        {
+            Console.WriteLine("[WARNING]: {0}", msg);
+        }
+    }
+}

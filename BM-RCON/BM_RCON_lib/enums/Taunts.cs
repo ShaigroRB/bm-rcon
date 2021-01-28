fa2ce22443bb37b0c180906a81d54f94abea7fdf
ShaigroRB <robin.boucher@epita.fr>
2021-01-29 00:52:30 +0100

    feat: add Taunt enum

---
+++ BM-RCON/BM_RCON_lib/enums/Taunts.cs
@@ -0,0 +1,14 @@
+﻿namespace BM_RCON.BM_RCON_lib
+{
+    /// <summary>
+    /// Enum for taunts
+    /// </summary>
+    enum Taunt
+    {
+        barf = 0,
+        smoke = 1,
+        drink = 2,
+        warcry = 3,
+        letsgo = 4
+    }
+}

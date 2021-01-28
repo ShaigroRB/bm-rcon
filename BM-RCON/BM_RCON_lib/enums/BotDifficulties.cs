2be139d0d7cf9cb90373434b84849866954e7300
ShaigroRB <robin.boucher@epita.fr>
2021-01-28 22:23:13 +0100

    feat: add GetString method to BotDifficulty enum

--- BM-RCON/BM_RCON_lib/enums/BotDifficulties.cs
+++ BM-RCON/BM_RCON_lib/enums/BotDifficulties.cs
@@ -12,4 +12,18 @@
         cruel = 3,
         random = 4
     }
+
+    static class BotDifficultyExtension
+    {
+        /// <summary>
+        /// Extension method to get the string associated to a BotDifficulty
+        /// </summary>
+        /// <param name="difficulty">The bot difficulty</param>
+        /// <returns>The bot difficulty as a readable string</returns>
+        public static string GetString(this BotDifficulty difficulty)
+        {
+            string[] difficulties = { "Easy", "Normal", "Hard", "Cruel", "Random" };
+            return difficulties[(int)difficulty];
+        }
+    }
 }

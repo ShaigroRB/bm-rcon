e4c03bfcb75379fc1fe0978cdc3391b016673fc2
ShaigroRB <robin.boucher@epita.fr>
2021-01-29 19:21:33 +0100

    feat: add Mutator enum

---
+++ BM-RCON/BM_RCON_lib/enums/Mutators.cs
@@ -0,0 +1,40 @@
+﻿namespace BM_RCON.BM_RCON_lib
+{
+    /// <summary>
+    /// Enum for mutator
+    /// </summary>
+    /// <description>Works with 'mutatorsetting' and 'setmutatorplayer'</description>
+    enum Mutator
+    {
+        /// <summary>Works with 'mutatorsetting' only</summary>
+        zombies = 2,
+        barf_mania = 4,
+        fall_damage = 5,
+        auto_reloading = 6,
+        amphibious_weapons = 7,
+        friendly_fire = 8,
+        max_health = 9,
+        dual_wielding = 10,
+        infinite_ammo = 11,
+        infinite_nades = 12,
+        double_jumping = 13,
+        infinite_o2 = 14,
+        weapon_behavior = 15,
+        random_weapons = 16,
+        tactical = 17,
+        perma_invisibility = 18,
+        perma_triple_damage = 19,
+        perma_super_speed = 20,
+        perma_regeneration = 21,
+        perma_poison = 22,
+        vampirism = 23,
+        explosive_death = 24,
+        headshots = 25,
+        /// <summary>Works with 'mutatorsetting' only.</summary>
+        team_size = 26,
+        random_deaths = 30,
+        alcoholics_anonymous = 31,
+        floor_is_lava = 32,
+        infinite_money = 33
+    }
+}

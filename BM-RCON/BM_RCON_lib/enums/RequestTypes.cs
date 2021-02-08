2f79f70af628fd08a1e7cc4cabe458404d16377f
ShaigroRB <robin.boucher@epita.fr>
2021-02-08 18:50:56 +0100

    docs: fix link to repos for doxygen

--- BM-RCON/BM_RCON_lib/enums/RequestTypes.cs
+++ BM-RCON/BM_RCON_lib/enums/RequestTypes.cs
@@ -1,14 +1,14 @@
 ﻿namespace BM_RCON.BM_RCON_lib
 {
     /// <summary>
-    /// Enum for request types (cf <see href="https://github.com/Spasman/rcon_example#sending-requests-and-processing-request_data">Spasman's docs</see>)
+    /// Enum for request types (cf <a href="https://github.com/Spasman/rcon_example#sending-requests-and-processing-request_data">Spasman's docs</a>)
     /// </summary>
     enum RequestType : short
     {
         login = 0,
         ping = 1,
         /// <summary>
-        /// Cf <see href="https://github.com/ShaigroRB/bm-boilerplate#additional-server-commands-you-can-send-these-packets-via-rcon-besides-the-request-data-ones">server commands</see> from Coyote's boilerplate
+        /// Cf <a href="https://github.com/ShaigroRB/bm-boilerplate#additional-server-commands-you-can-send-these-packets-via-rcon-besides-the-request-data-ones">server commands</a> from Coyote's boilerplate
         /// </summary>
         command = 2,
         request_player = 3,
